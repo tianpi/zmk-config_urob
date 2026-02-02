ee9239ce0c17e3c0e288089a2836101dfc8437cb
Tian Pi <5852961+tianpi@users.noreply.github.com>
2026-02-02 17:00:41 +0100

    go60: scrollpads (aka, cirques)

--- config/moergo_includes.h
+++ config/moergo_includes.h
@@ -7,26 +7,22 @@
 
 #pragma once
 
+////////////////////////////////////////////////////////////////
+// defaults
+
 #include <behaviors.dtsi>
-#include <behaviors/num_word.dtsi>
 
 #include <dt-bindings/zmk/keys.h>
-#include <dt-bindings/zmk/ext_power.h>
 #include <dt-bindings/zmk/outputs.h>
-#include <dt-bindings/zmk/rgb.h>
-#include <dt-bindings/zmk/input_transform.h> // go60
-#include <dt-bindings/zmk/pointing.h> // go60
-
-#define CONFIG_WIRELESS 1
-#if CONFIG_WIRELESS
 #include <dt-bindings/zmk/bt.h>
-#endif
+#include <dt-bindings/zmk/rgb.h>
+#include <dt-bindings/zmk/ext_power.h>
 
-#include <input/processors.dtsi> // go60
+////////////////////////////////
+// extras
 
-#include <zephyr/dt-bindings/input/input-event-codes.h> // go60
-
-#include "zmk-helpers/helper.h"
+#include <behaviors/num_word.dtsi>
+#include <zmk-helpers/helper.h>
 
 ////////////////////////////////////////////////////////////////
 
